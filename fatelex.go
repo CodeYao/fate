b578ab37d289c4bd69026635d835ea3aff4b6ecd
Codeyao <457875141@qq.com>
2018-06-11 17:15:58 +0800

    词法分析进行中

--- fatelex.go
+++ fatelex.go
@@ -6,11 +6,47 @@ import (
 	"os"
 )
 
+var fileLength int = 0
+var currentChar int = 0
+var tokenList []Token
+var fileByte []byte
+
 func main() {
-	fileByte := readFile2Byte("fatetest.fate")
-	fmt.Println(string(fileByte))
+	fileByte = readFile2Byte("test.fate")
+	fileLength = len(fileByte)
+	Lex(fileByte)
+	//fmt.Println(string(fileByte))
 }
 
+func Lex(fileByte []byte) {
+	for {
+		if currentChar == fileLength {
+			break
+		}
+		c := fileByte[currentChar]
+		currentChar += 1
+		switch {
+		case isSpace(int(c)):
+		case isComment(int(c)):
+			skipComment()
+			fmt.Println("This is a space or comment chenyao, and the value is ", string(c))
+		case isNewline(int(c)):
+			skipNewline()
+			fmt.Println("This is a new line chenyao")
+		case isAlpha(int(c)):
+			getToken(c)
+			fmt.Println("This is a Letter or _ chenyao", string(c))
+		}
+		//fmt.Println(string(c))
+	}
+	// reg := regexp.MustCompile(`\d+\.\d+|\w+|\d+`)
+	// file := reg.FindAll(fileByte, -1)
+	// for _, v := range file {
+	// 	println(string(v))
+	// }
+}
+
+//读取文件
 func readFile2Byte(path string) []byte {
 	f, err := os.Open(path)
 	if err != nil {
@@ -26,4 +62,70 @@ func readFile2Byte(path string) []byte {
 	}
 
 	return fd
+}
+
+// 跳过注释
+func skipComment() {
+	for {
+		currentChar += 1
+		c := fileByte[currentChar]
+		if isNewline(int(c)) || isEof(int(c)) {
+			currentChar -= 1
+			return
+		}
+	}
+}
+
+// 跳过空行
+func skipNewline() {
+	for {
+		currentChar += 1
+		if currentChar == fileLength {
+			return
+		}
+		c := fileByte[currentChar]
+		if !isNewline(int(c)) {
+			currentChar -= 1
+			return
+		}
+	}
+}
+
+//获取token
+func getToken(c_char byte) {
+	tokenName := []byte{c_char}
+	for {
+		c := fileByte[currentChar]
+		currentChar += 1
+		if !isAlnum(int(c)) && !isAlpha(int(c)) {
+			currentChar -= 1
+			var token Token
+			token.tokenName = string(tokenName[:])
+			if isKeyWord(token.tokenName) {
+				fmt.Println("'", token.tokenName, "'\t", token.tokenName)
+				break
+			} else {
+				fmt.Println("<IDENTIFIER>\t", token.tokenName)
+				break
+			}
+		} else {
+			tokenName = append(tokenName, c)
+		}
+	}
+}
+
+//获取数字
+
+//获取字符
+
+//获取字符串
+
+//判断是否关键字
+func isKeyWord(tokenName string) bool {
+	for _, v := range KeyWords {
+		if tokenName == v {
+			return true
+		}
+	}
+	return false
 }
---
+++ fatetype.go
@@ -0,0 +1,52 @@
+package main
+
+type TokenType int
+
+const (
+	NULL                TokenType = iota //空字符
+	EXCEPTION                            //异常字符
+	NUMBER_TOKEN                         //数字标识符
+	CHAR_TOKEN                           //字符标识符
+	CHAR_SIGN_TOKEN                      //单引号'标识符
+	STRING_TOKEN                         //字符串标识符
+	STRING_SIGN_TOKEN                    //双引号"标识符
+	BOOL_TOKEN                           //布尔标识符
+	IF_TOKEN                             //if标识符
+	ELSE_TOKEN                           //else标识符
+	FOR_TOKEN                            //for标识符
+	FUNC_TOKEN                           //func标识符
+	PARAM_TOKEN                          //变量标识符
+	STATE_TOKEN                          //声明变量标识符
+	STATE_TYPE_TOKEN                     //声明类型标识符let,set
+	TOKEN_TYPE_TOKEN                     //变量类型标识符int,float...
+	IDENTIFIER_TOKEN                     //变量
+	ADD_OPERATOR_TOKEN                   //加法+
+	SUB_OPERATOR_TOKEN                   //减法-
+	MUL_OPERATOR_TOKEN                   //乘法*
+	DIV_OPERATOR_TOKEN                   //除法/
+	MOD_OPERATOR_TOKEN                   //取余%
+	ASS_OPERATOR_TOKEN                   //赋值=
+	LEFT_PAREN_TOKEN                     //左括号(
+	RIGHT_PAREN_TOKEN                    //右括号)
+	LEFT_BRACKET_TOKEN                   //左中括号[
+	RIGHT_BRACKET_TOKEN                  //右中括号]
+	LEFT_BRACES_TOKEN                    //左大括号{
+	RIGHT_BRACES_TOKEN                   //右大括号}
+	END_OF_LINE_TOKEN                    //行结束符;
+	EQ_TOKEN                             // ==
+	NE_TOKEN                             // !=
+	GT_TOKEN                             // >
+	GE_TOKEN                             // >=
+	LT_TOKEN                             // <
+	LE_TOKEN                             // <=
+	LOGICAL_AND_TOKEN                    // &&
+	LOGICAL_OR_TOKEN                     // ||
+
+)
+
+var KeyWords = []string{"int8", "int16", "int32", "int64", "uint8", "uint16", "uint32", "uint64", "bool", "float32", "float64", "string", "char", "if", "else", "for", "break", "return", "let", "set", "func", "false", "true"}
+
+type Token struct {
+	tokenName string
+	tokenType TokenType
+}
